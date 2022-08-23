5fe94762ca1891546982543b12420a8d3b057857
prijacash <prijacash@gmail.com>
2022-08-23 10:38:12 -0700

    updated display health bars

--- scripts/script.js
+++ scripts/script.js
@@ -4,26 +4,23 @@ console.log('test')
 window.addEventListener("DOMContentLoaded", () => {
 
     // Capture DOM Elements from page to store as variables
-    let cpuHealth = document.querySelector('cpuHealth')
-    let playerHealth = document.querySelector('playerHealth')
-    let attack1 = document.querySelector('attack1')
-    let attack2 = document.querySelector('attack2')
-    let attack3 = document.querySelector('attack3')
-    let displayMessage = document.querySelector('displayMessage')
+    let cpuHealth = document.querySelector('#cpuHealth')
+    let playerHealth = document.querySelector('#playerHealth')
+    let attack1 = document.querySelector('.attack1')
+    let attack2 = document.querySelector('.attack2')
+    let attack3 = document.querySelector('.attack3')
+    let displayMessage = document.querySelector('.displayMessage')
     
 
     // display initial message
-    message.innerText = "Player 1, goes first to attack"
+    displayMessage.innerText = 'Player 1, goes first to attack'
 
     // SCOREBOARD
 
-    let turnNum: 0
-    let player1: [] // array of player attacks
-    let cpu: [] // array of cpu attacks
+    let turnNum = 0
+    let player1, cpu; 
 
-    let player: 
-
-    characters = [
+    const characters = [
         {
             name: "Charizard",
             image: "",
@@ -31,69 +28,95 @@ window.addEventListener("DOMContentLoaded", () => {
                 {
                 current: 500,
                 total: 500
-                } 
+                }, 
            
             attacks: [
                 {
                     attackName: "Ember Fire",
-                    hp: randomNum(60,30),
+                    damage: randomNum(60,30),
                     available:  10,
                     remaining: 10,
                 },
                 {
                     attackName: "Fire Spin",
-                    hp: randomNum(45,12),
+                    damage: randomNum(45,12),
                     available:  10,
                     remaining: 10, 
-                }
+                },
                 {
                     attackName: "Blast Burn",
-                    hp: randomNum(30,10),
+                    damage: randomNum(30,10),
                     available:  10,
                     remaining: 10,
-                }
-            ]
-        }
+                },
+            ],
+        },
         {
-            name: "Snorlax"
+            name: "Snorlax",
             image: "",
             hp: 
                 {
                     current: 500,
                     total: 500,
-                }   
+                },   
             attacks: [
                 {
                     attackName: "Earthquake",
-                    hp: randomNum(60,30),
+                    damage: randomNum(60,30),
                     available:  10,
                     remaining: 10,
                 },
                 {
                     attackName: "Heavy Slam",
-                    hp: randomNum(45,12),
+                    damage: randomNum(45,12),
                     available:  10,
                     remaining: 10,  
-                }
+                },
                 {
                     attackName: "Skull Bash",
-                    hp: randomNum(30,10),
+                    damage: randomNum(30,10),
                     available:  10,
                     remaining: 10,
-                }
-            ]
+                },
+            ],
                 
-        }
+        },
     ]
 
+    // delare players to data set
+    player1 = characters[0]
+    cpu = characters[1]
+
+    // display initial health bars
+    playerHealth.innerText = `Player 1 ${player1.name} Health (${player1.hp.current} / ${player1.hp.total})`
+    cpuHealth.innerText = `CPU ${cpu.name} Health (${cpu.hp.current} / ${cpu.hp.total})`
+
     // FUNCTION: attackClicked() ---> starts
+    
+    attack1.addEventListener("click", attackClick)
+    attack2.addEventListener("click", attackClick)
+    attack3.addEventListener("click", attackClick)
+
+
     function attackClick(event) {
         let gameCheck = event.target
 
-            // decide who's turn it is.. player1 even, cpu odd
+            // 1. check turn. decide who's turn it is.. player1 even, cpu odd
             if (turnNum % 2 === 0) {
                 gameCheck.classList.add('player1')
                 displayMessage.innerText = "Player 1's turn"
+                if (player1.hp.current > 0) { // which buttons being clicked, based the same button action
+                    if (attack1) {
+                        cpu.hp.current =  cpu.hp.current - player1.attacks[0].damage               
+                    } else if (attack2) {
+                        cpu.hp.current = cpu.hp.current - player1.attacks[1].damage
+                    } else if (attack3) {
+                        cpu.hp.current = cpu.hp.current - player1.attacks[2].damage
+                    }
+                    // reassign the HP
+         
+                } 
+
                 turnNum++
             } else {
                 gameCheck.classList.add('cpu')
@@ -101,24 +124,32 @@ window.addEventListener("DOMContentLoaded", () => {
                 turnNum++
             }
 
+            // 2. function to attack
+            // first conditional check HP > 0
+    
+
+ 
+            // gameOver()
+            // console.log('this game is over)
+            
+            // 3. take HP of that guy
+            
+            // (total HP combatant) - (total attack points from attacker)
+            // 4. update turn
+            // repeat go back to one
 
     }
-        // (total HP combatant) - (total attack points from attacker)
-        // add
 
-        // 1. check turn
-        // 2. function to attack
-        // 3. take HP of that guy
-        // 4. update turn
-        // repeat go back to one
 
     // random number generator for attacks
     function randomNum(max, min){
-        if (min === undefined || min === '' || min === null) {
-            min = 0
-        }
-        return Math.floor(Math.random() * (max-min) + min);
+    if (min === undefined || min === '' || min === null) {
+        min = 0
     }
+    return Math.floor(Math.random() * (max-min) + min);
+    randomNum()
+    }
+
 
     // FUNCTION: gameOver()
     function gameOver () {
@@ -129,3 +160,5 @@ window.addEventListener("DOMContentLoaded", () => {
 
     // FUNCTION: stopGame() --> doesnt allow further game play
 })
+
+
